12a26d888c1059d8f393415552cae9226bd619bd
Richard Alvin <richardalvinpratama8@gmail.com>
2024-10-03 23:47:28 +0700

    note search and adjust header

--- src/NoteHeader.jsx
+++ src/NoteHeader.jsx
@@ -5,7 +5,14 @@ class NoteHeader extends React.Component {
         return (
             <div className="note-app__header">
                 <h1>RAPNote</h1>
-                <input type="text" />
+                <div className="note-app__header-left">
+                    <a href="https://github.com/RichardAlvin">
+                        <img src="/github.png" height="40px"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/richard-alvin-pratama/">
+                        <img src="/linkedin.png" height="40px"/>
+                    </a>
+                </div>
             </div>
         );
     }
--- src/NoteList.jsx
+++ src/NoteList.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
 import NoteItem from './components/NoteCard/NoteItem';
  
-function NoteList({ notes, onDelete, onArchive }) {
-    const activeNotes = notes.filter((note) => note.archived === false);
+class NoteList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchNote: ''
+        };
+    }
 
-    return (
-        <div className="note-list-div">
-            <h2>Active Note</h2>
-            <div className="notes-list note-list-active-div">
-            {
-                activeNotes.length > 0 ? (
-                    activeNotes.map((note) => (
-                        <NoteItem 
-                        key={note.id}
-                        onDelete={onDelete} 
-                        onArchive={onArchive}
-                        {...note} />
-                    ))
-                ) : (
-                    <p>Empty Notes</p>
-                )
-            }
+    handleSearchNote = (e) => {
+        this.setState({ searchNote: e.target.value });
+    }
+
+    render() {
+        const { notes, onDelete, onArchive } = this.props;
+
+        const activeNotes = notes.filter((note) => note.archived === false)
+        .filter((note) => 
+            note.title.toLowerCase().includes(this.state.searchNote.toLowerCase())
+        )
+        .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
+        return (
+            <div className="note-list-div">
+                <div className="note-list-div-header">
+                    <h3>Active Note</h3>
+                    <input type="text" placeholder='Search...' value={this.state.searchNote} onChange={this.handleSearchNote}/>
+                </div>
+                <div className="notes-list note-list-active-div">
+                {
+                    activeNotes.length > 0 ? (
+                        activeNotes.map((note) => (
+                            <NoteItem 
+                            key={note.id}
+                            onDelete={onDelete} 
+                            onArchive={onArchive}
+                            {...note} />
+                        ))
+                    ) : (
+                        <p>Empty Notes</p>
+                    )
+                }
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
  
 export default NoteList;
