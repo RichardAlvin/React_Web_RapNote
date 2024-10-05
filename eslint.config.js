6eaed4414e51aeb823f5922587d67e8a1ccab439
Richard Alvin <richardalvinpratama8@gmail.com>
2024-10-05 10:45:02 +0700

    remove unused package

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
-import daStyle from 'eslint-config-dicodingacademy';
 
 
 export default [
-  daStyle,
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
